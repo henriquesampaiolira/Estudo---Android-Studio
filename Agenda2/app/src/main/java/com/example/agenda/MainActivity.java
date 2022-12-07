4132d1b2bb1046ff974f8a0d514691b20ef0824e
Henrique Sampaio Lira <84481479+henriquesampaiolira@users.noreply.github.com>
2022-12-06 23:19:02 -0300

    Criando Agenda com banco de dados local

--- Agenda2/app/src/main/java/com/example/agenda/MainActivity.java
+++ Agenda2/app/src/main/java/com/example/agenda/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.agenda;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,11 +9,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;//import do banco de dados
+import android.database.Cursor;//navegar entre os registros
+import android.widget.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
     EditText et_nome, et_telefone;
     Button btn_gravar,btn_fechar,btn_consultar;
+
+    SQLiteDatabase db=null;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         btn_fechar      =findViewById(R.id.btn_fechar);
         btn_gravar      =findViewById(R.id.btn_gravar);
 
+
+        criarAbrirDB();
+    }
+
+    public void criarAbrirDB(){
+        try {
+            db=openOrCreateDatabase("bancoAgenda",MODE_PRIVATE,null);
+        } catch (Exception ex){
+            msg("Erro ao criar banco de dados.");
+        }
+        try {
+            db.execSQL("CREATE TABLE IF NOT EXISTS contatos (id INTEGER PRIMARY KEY,nome TEXT,fone TEXT)");
+        } catch (Exception ex){
+            msg("Erro ao criar tabela.");
+        }
+        finally {
+            msg("Tabela criada com sucesso.");
+        }
+    }
+
+    public void fecharDB(){
+        db.close();
     }
 
     public void mudarTelaConsulta(View v){
@@ -33,4 +62,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(itTelaConsulta);
     }
 
+    public void msg(String v){
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setMessage(v);
+        adb.setNeutralButton("OK",null);
+        adb.show();
+    }
 }
